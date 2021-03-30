20c421725aae48ed0c97b4b05773d8ef71927b8c
atenaiis <attennaisc@gmail.com>
2021-03-30 11:52:04 -0600

    Revert "revert changes"
    This reverts commit cf6784db2063a44bfe17d393421f3469267e57d2.

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-has_many :posts        
-end
