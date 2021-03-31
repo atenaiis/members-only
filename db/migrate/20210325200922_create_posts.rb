18249d855f1cb5aa790cdaf04e4fa790e02eebb9
atenaiis <attennaisc@gmail.com>
2021-03-31 15:47:16 -0600

    fix user migrate

--- db/migrate/20210325200922_create_posts.rb
+++ db/migrate/20210325200922_create_posts.rb
@@ -6,6 +6,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
-    end
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_03_30_183759) do
 
   create_table "posts", force: :cascade do |t|
-    t.text "post"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
