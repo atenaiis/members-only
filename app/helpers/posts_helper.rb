998ca603fc405e34398cbc35e8931fee0cb7db94
atenaiis <attennaisc@gmail.com>
2021-04-01 09:04:03 -0600

    work on requested changes on code review

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,3 +11,13 @@ module PostsHelper
     return 'd-none' unless user_signed_in? && current_user.id == post.user_id
   end
 end
+def navbar
+  if user_signed_in?
+  link_to('Edit profile', edit_user_registration_path, class: 'navbar-link strong' ) + 
+  link_to('Sign Out', destroy_user_session_path, method: :delete, class: 'nav-link glyphicon glyphicon-log-in text-white') +
+  content_tag(:a, current_user.name, class: %w[nav-link disabled])
+  else
+  link_to('Sign In', user_session_path, class: 'nav-link active glyphicon glyphicon-log-in text-white') +
+  link_to('Sign Up', new_user_registration_path, class: 'nav-link active glyphicon glyphicon-user text-white')
+  end
+  end
